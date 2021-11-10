2053fba2f32dfad7df5138fe139489374ec48573
SingerDavid <dsinger1@hawk.iit.edu>
2021-11-09 22:54:25 -0600

    Describe initial layout of webRTC, DOM, and Signaling events

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,27 @@
+'use strict'
+
+const $self = {
+  rtcConfig: null,
+  mediaConstrains: { audio: false, video: true }
+};
+
+const $peers = {
+
+};
+
+/* Set up a stream by grabbing self and peers */
+
+/* DOM media events */
+
+/* Socket Server Events and Callbacks*/
+
+/* DOM Events */
+
+/* WebRTC Events */
+
+/* Signaling Channel Events */
+
+
 /**
 David start
 */
